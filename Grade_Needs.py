3a5d96e6424f822db51bb0b5a566d2ba398a22ab
Paulo Mariano <42322804+PauloHMariano@users.noreply.github.com>
2018-10-07 21:15:39 -0300

    Update Grade_Needs.py

--- Grade_Needs.py
+++ Grade_Needs.py
@@ -1,15 +1,15 @@
 import numpy as np
-nota1 = float(input('Nota 1 '))
-nota2 = float(input('Nota 2 '))
+nota1 = float(input('Input Grade 1 '))
+nota2 = float(input('Input Grade 2 '))
 getit = [nota1, nota2]
 media = np.mean(getit)
 
 if media < 5.0:
-    print('Você está reprovado. Sua nota é {}.'.format(media))
+    print('Your not approved. Your grade is {}.'.format(media))
 elif (media >= 5.0) and (media <=6.9):
-    print('Recuperação. Sua nota é {}.'.format(media))
+    print('Retake test needed. Your grade is {}.'.format(media))
 else:
-    print('Aprovado, sua nota é {}.'.format(media))
+    print('Approved, your grade is {}.'.format(media))
 
 
 
